a6a7b4013f1259b7b13d2c02549774e51791f4f7
xiaonanpols21 <74655427+xiaonanpols21@users.noreply.github.com>
2026-07-27 13:58:15 +0200

    bezig result page

--- src/app/confirm/page.js
+++ src/app/confirm/page.js
@@ -1,7 +1,26 @@
+"use client";
+import { useState, useEffect } from "react";
 import Image from "next/image";
 import styles from "./styles.module.scss";
 
 export default function Confirm() {
+    const [form, setForm] = useState("");
+
+    useEffect(() => {
+        const savedForm = localStorage.getItem("form");
+
+          if (savedForm) {
+            setForm(JSON.parse(savedForm));
+        }
+    }, []);
+
+    console.log(form)
+
+    const activity =
+    form.activity ||
+    form.randomActivity ||
+    form.customActivity;
+
     return (
         <section className={styles.confirm}>
             <div className="wrapper">
@@ -13,9 +32,39 @@ export default function Confirm() {
                 <ul>
                     <li>
                         <p>Date</p>
-                        <p>03/06/2027</p>
+                        <p>{form.date}</p>
                         <Image
                             src="/img/svg/agenda.svg"
+                            width={35}
+                            height={35}
+                            alt="Picture of the author"
+                        />
+                    </li>
+                    <li>
+                        <p>Time</p>
+                        <p>{form.time}</p>
+                        <Image
+                            src="/img/svg/clock.svg"
+                            width={35}
+                            height={35}
+                            alt="Picture of the author"
+                        />
+                    </li>
+                    <li>
+                        <p>Activity</p>
+                        <p>{activity}</p>
+                        <Image
+                            src="/img/svg/activity.svg"
+                            width={35}
+                            height={35}
+                            alt="Picture of the author"
+                        />
+                    </li>
+                    <li>
+                        <p>Excitement</p>
+                        <p>{form.excitement}</p>
+                        <Image
+                            src="/img/svg/heart.svg"
                             width={35}
                             height={35}
                             alt="Picture of the author"
